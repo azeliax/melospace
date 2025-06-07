fa3bf33a785a9cc3fec391a59b03b41f3f7afb18
Natalia Wietecha <natalia497w@gmail.com>
2025-06-07 11:49:26 +0200

    test render

--- src/Login.js
+++ src/Login.js
@@ -12,7 +12,7 @@ export default function Login() {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-          const res = await axios.post('http://localhost:5000/login', form);
+          const res = await axios.post('http://https://melospace.onrender.com/login', form);
     
           console.log('Response from server:', res.data);
     
