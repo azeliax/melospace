0372ae10afd9b4170c6f1a690ac8f2a27417618e
Natalia Wietecha <natalia497w@gmail.com>
2025-06-07 12:52:57 +0200

    fixing bcryptjs not found 3

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const { Client } = require('pg');
 
 const app = express();
