cc1c235bfd44e8c573a2a9d873eb09142c979ef7
Natalia Wietecha <natalia497w@gmail.com>
2025-06-14 23:18:33 +0200

    session test

--- backend/index.js
+++ backend/index.js
@@ -34,7 +34,8 @@ app.use(session({
   saveUninitialized: false,
   cookie: {
     secure: false,
-    maxAge: 24 * 60 * 60 * 1000
+    maxAge: 24 * 60 * 60 * 1000,
+    sameSite: 'lax',
   }
 }));
 
@@ -135,6 +136,7 @@ app.post('/addtoplaylist', async (req, res) => {
 
 app.get('/getusername', async (req, res) => {
   try {
+    console.log('Session data:', req.session);
     const userId = req.session.user_id;
     const result = await db.query('SELECT username FROM public."Users" WHERE user_id = $1', [userId]);
     res.json({username: result.rows[0]?.username});
